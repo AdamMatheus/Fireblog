3534b1f2ecbf6c05c4b33cde606a69d144146a12
AdamMatheus <adam.matheus.it@gmail.com>
2022-02-03 17:58:20 +0100

    ok

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
-import AuthContextProvider from "./context/AuthContextProvider";
-import AppRouter from "./router/AppRouter";
-import { ToastContainer } from "react-toastify";
-import { BlogContextProvider } from "./context/BlogContextProvider";
-function App() {
+import Navbar from "./components/Navbar";
+
+function App () {
   return (
-    <div className="App">
-      <AuthContextProvider>
-        <BlogContextProvider>
-          <AppRouter />
-          <ToastContainer />
-        </BlogContextProvider>
-      </AuthContextProvider>
-    </div>
+  <div className="App">
+
+    <Navbar/>
+
+  </div>
   );
-}
+};
 
 export default App;
--- solution/src/index.js
+++ solution/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
